8d8941ecffe2b781fc9bde6d42d58d199c76c7dc
amy <amarantakruger@gmail.com>
2022-10-14 16:00:40 +0200

    read single.php+header

--- config/Database.php
+++ config/Database.php
@@ -12,12 +12,14 @@ class Database
     public function connect()
     {
         $this->conn = null;
+
         try {
-            $this->conn = new PDO('mysql:host=' . $this->host . ';dbname= ' . $this->db_name, $this->username, $this->password);
+            $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             echo 'Connection Error: ' . $e->getMessage();
         }
+
         return $this->conn;
     }
 }
--- models/Post.php
+++ models/Post.php
@@ -1,42 +1,115 @@
 <?php
-//DB stuff
+
+// Read Function
+
 class Post
 {
-    private $conn;
-    private $table = 'posts';
+  // DB stuff
+  private $conn;
+  private $table = 'posts';
 
-    // Post Properties
-    public $id;
-    public $category_id;
-    public $category_name;
-    public $title;
-    public $body;
-    public $author;
-    public $created_at;
+  // Post Properties
+  public $id;
+  public $category_id;
+  public $category_name;
+  public $title;
+  public $body;
+  public $author;
+  public $created_at;
 
-    // Constructor with DB
-    public function __construc($db)
-    {
-        $this->conn = $db;
+  // Constructor with DB
+  public function __construct($db)
+  {
+    $this->conn = $db;
+  }
+
+  // Get Posts
+  public function read()
+  {
+    // Create query
+    $query = 'SELECT c.name as category_name, p.id, p.category_id, p.title, p.body, p.author, p.created_at
+        FROM ' . $this->table . ' p
+          LEFT JOIN
+          categories c ON p.category_id = c.id
+          ORDER BY
+          p.created_at DESC';
+
+    // Prepare statement
+    $stmt = $this->conn->prepare($query);
+
+    // Execute query
+    $stmt->execute();
+
+    return $stmt;
+  }
+  ///////////////////////////////////
+
+  // Get Single Post
+  public function single_post()
+  {
+    // Create query
+    $query = 'SELECT c.name as category_name, p.id, p.category_id, p.title, p.body, p.author, p.created_at
+    FROM ' . $this->table . ' p
+    LEFT JOIN
+    categories c ON p.category_id = c.id
+    WHERE
+    p.id = ?
+    LIMIT 0,1';
+
+    // Prepare Statement
+    $stmt = $this->conn->prepare($query);
+
+    // Bind ID
+    $stmt->bindParam(1, $this->id);
+
+    // Execute query
+    $stmt->execute();
+
+    // Fetch Array
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    // Set Properties
+    $this->title = $row['title'];
+    $this->body = $row['body'];
+    $this->author = $row['author'];
+    $this->category_id = $row['category_id'];
+    $this->category_name = $row['category_name'];
+  }
+
+  // Create Post
+  public function create()
+  {
+    // Create query
+    $query = 'INSERT INTO ' . $this->table . '
+    SET
+      title = :title,
+      body = :body,
+      author = :author,
+      category_id = :category_id';
+
+    // Prepare Statement
+    $stmt = $this->conn->prepare($query);
+
+    // Clean data
+    $this->title = htmlspecialchars(strip_tags($this->title));
+    $this->body = htmlspecialchars(strip_tags($this->body));
+    $this->authot = htmlspecialchars(strip_tags($this->author));
+    $this->title = htmlspecialchars(strip_tags($this->category_id));
+
+    // Bind data
+    $stmt->bindParam(':title', $this->title);
+    $stmt->bindParam(':body', $this->body);
+    $stmt->bindParam(':author', $this->author);
+    $stmt->bindParam(':category_id', $this->category_id);
+
+    // Execute query
+    if ($stmt->execute()) {
+      return true;
     }
 
-    // Get Posts
-    public function read()
-    {
-        // Create query
-        $query = 'SELECT c.name as category_name, p.id, p.category_id, p.title, p.body, p.author, p.created_at
-            FROM ' . $this->table . ' p
-            LEFT JOIN
-            categories c ON p.category_id = c.id
-            ORDER BY
-            p.created_at DESC';
+    // Print error if something goes wrong
+    printf("Error: %s.\n", $stmt->error);
 
-        //Prepare statement
-        $stmt = $this->conn->prepare($query);
-
-        //Execute quey
-        $stmt->execute();
-
-        return $stmt;
-    }
+    return false;
+  }
 }
