220bcbad0d532953be4a62fdd565de2e0f7ec1b5
amy <amarantakruger@gmail.com>
2022-10-14 10:55:13 +0200

    init

---
+++ config/Database.php
@@ -0,0 +1,10 @@
+<?php
+class Database
+{
+    // DB Params
+    private $host = 'localhost';
+    private $db_name = 'myblog';
+    private $username = 'root';
+    private $password = '123456';
+    private $conn;
+}
